ceaf4f1f88f9d277aafca706bcd7e3fdbd2f40cd
Deep Learning <kvoitiuk@ucsc.edu>
2019-03-27 16:07:38 -0700

    Debugging startup script again

--- src/setup.sh
+++ src/setup.sh
@@ -3,7 +3,7 @@
 job="@reboot $HOME/experiment/src/startup.sh &"
 # If not present, add startup cron job on machine:
 if ! [crontab -l | grep -q -F job]; then
-    crontab -l ; echo job | crontab -
+    crontab -l | {cat; echo job } | crontab -
     echo "Added cron job: "
     echo $job
 fi
