7ec99b835cd27e80d25738d50aa8e5f771a94a5c
Deep Learning <kvoitiuk@ucsc.edu>
2019-03-27 17:46:56 -0700

    Updated arrayWrapper.py again

--- src/arrayWrapper.py
+++ src/arrayWrapper.py
@@ -193,7 +193,7 @@ def configuredExperiment(inputArray, filepath):
 
 
 def get_ip_address():
-    f = os.popen('ifconfig wlan0 | grep "inet\ addr" | cut -d: -f2 | cut -d" " -f1')
+    f = os.popen('ifconfig wlan0 | grep "inet " | awk '{print $2}')
     return f.read()
 
 
