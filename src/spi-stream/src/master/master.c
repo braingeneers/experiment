24690d5d6adef031a092ee375744170ed82245b6
Kateryna Voitiuk <kvoitiuk@ucsc.edu>
2019-06-23 23:42:09 -0700

    SPI loopback test

--- src/spi-stream/src/master/master.c
+++ src/spi-stream/src/master/master.c
@@ -6,12 +6,38 @@
 //-----------------------------------------------------------------------------
 
 
-#include "master.h"
+#include "../protocol.h"
 
 
 
 int main(int argc, char *argv[]) {
 
-    return 0;
+  if (!bcm2835_init())  {
+    printf("bcm2835_init failed; running as root??\n");
+    exit(1);
+  }
+
+  if (!bcm2835_spi_begin()) {
+    printf("bcm2835_spi_begin failed; running as root??\n");
+    exit(1);
+  }
+
+  bcm2835_spi_setBitOrder(BCM2835_SPI_BIT_ORDER_MSBFIRST);      // default
+  bcm2835_spi_setDataMode(BCM2835_SPI_MODE0);                   // default
+  bcm2835_spi_setClockDivider(BCM2835_SPI_CLOCK_DIVIDER_65536); // default
+  bcm2835_spi_chipSelect(BCM2835_SPI_CS0);                      // default
+  bcm2835_spi_setChipSelectPolarity(BCM2835_SPI_CS0, LOW);      // default
+
+  // Send a byte to slave and simultaneously read a byte back from slave
+  uint8_t send_data, read_data;
+  send_data = 0x23;
+  read_data = bcm2835_spi_transfer(send_data);
+
+  printf("Sent to SPI: 0x%02X. Read from SPI: 0x%02X.\n", send_data, read_data);
+
+  //exit
+  bcm2835_spi_end();
+  bcm2835_close();
+  return 0;
 
 }
--- src/spi-stream/src/protocol.h
+++ src/spi-stream/src/protocol.h
@@ -1,7 +1,7 @@
 //-----------------------------------------------------------------------------
 // Kateryna Voitiuk (kvoitiuk@ucsc.edu)
 // Braingeneers
-// master.h
+// protocol.h
 // Description: Header file for master spi controller
 //-----------------------------------------------------------------------------
 
@@ -10,7 +10,15 @@
 #define _MASTER_H
 
 #include <bcm2835.h>
+#include <stdio.h>
 
+//P1-19 (MOSI)
+//P1_21 (MISO)
+//P1-23 (CLK)
+//P1-24 (CE0)
+//P1-26 (CE1)
+
+//don't expext speed faster than 31MHz to work reliably on RasPi
 
 
 #endif  /*_MASTER_H*/
