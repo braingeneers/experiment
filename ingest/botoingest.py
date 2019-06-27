ff6c4f0c2a0011c85d57ae44b39182a6ee302a3c
Nick Rezaee <37967537+resace3@users.noreply.github.com>
2019-06-27 15:40:32 -0700

    Update botoingest.py

--- ingest/botoingest.py
+++ ingest/botoingest.py
@@ -1,3 +1,4 @@
+import shutil
 import os
 import re
 import sys
@@ -6,12 +7,16 @@ import json
 import argparse
 import datetime
 import numpy as np
-import smtplib
-import datetime
+
 import read_data
-import shutil
-#The following code will ingest an rhd file in a directory. Then once it has ingested it, it will create a file in
-# derived/UUID with the correct file formats and create an original folder with all the original files.
+
+parser = argparse.ArgumentParser(
+    description="Ingest a batch of experiments")
+#parser.add_argument('--uuid', required=True,
+#                    help="UUID for batch")
+parser.add_argument('--issue', required=True,
+                   help="Github issue is in internal")
+args = parser.parse_args()
 
 import boto3
 import botocore
@@ -35,8 +40,9 @@ def download_directory_from_s3(bucketName,remoteDirectoryNames):
 
             if not os.path.exists(os.path.dirname(key.key)):
                 os.makedirs(os.path.dirname(key.key))
+          #  print("The Directory being made: "+ os.path.dirname(key.key) +" What is being downloaded in it: "+ key.key)
             bucket.download_file(key.key,key.key)
-            
+
 def copy_directory(src, dest):
     try:
         shutil.copytree(src, dest)
@@ -47,30 +53,26 @@ def copy_directory(src, dest):
     except OSError as e:
         print('Directory not copied. Error: %s' % e)
 
+remoteDirectories=['asrobbin@ucsc.edu']
+
+formerPath=os.getcwd()
+print("In Directory : " +os.getcwd())
+path="%s/original" %str(os.getcwd())
+os.chdir(path)
+print("In Directory : " +os.getcwd())
+download_directory_from_s3(BUCKET_NAME,remoteDirectories)
+os.chdir(formerPath)
+print("In Directory : "+ os.getcwd())
+#for remoteDirectory in remoteDirectories:
+ #   copy_directory(remoteDirectory,"original/{}".format(now.strftime("%Y-%m-%d")+"-"+remoteDirectory.partition("@")[0], exist_ok=True))
 
 
 
-
-#parserargparse.ArgumentParser(
-    #description="Ingest a batch of experiments")
-#parser.add_argument('--uuid', required=True,
- #                 help="UUID for batch")
-
-#parser.add_argument('--issue', required=True,
- #                   help="Github issue is in internal")
-#args = parser.parse_args()
+for remoteDirectory in remoteDirectories:
+    os.rename("original/{}".format(remoteDirectory),"original/{}".format(now.strftime("%Y-%m-%d")+"-"+remoteDirectory.partition("@")[0]))
 
 
 # Batch = set of folders each with an experiment and all its recordings
-remoteDirectories=['resace3@gmail.com','rcurrie@ucsc.edu','fakeEmail@fake.com']
-
-#downloading all emaisl from bucket
-download_directory_from_s3(BUCKET_NAME,remoteDirectories)
-for remoteDirectory in remoteDirectories:
-    copy_directory(remoteDirectory,"original/{}".format(now.strftime("%Y-%m-%d")+"-"+remoteDirectory.partition("@")[0], exist_ok=True))
-    files=os.listdir("original/{}".format(now.strftime("%Y-%m-%d")+"-"+ remoteDirectory.partition("@")[0]))
-    print("|||||||||||||||||||||||||||||||||||||||||||||this is the number of files in the email: "+ str(len(files))+ " and these are the files" + str(files))
-
 for remoteDirectory in remoteDirectories:
     print("Ingesting batch {}".format(remoteDirectory))
 
@@ -83,51 +85,69 @@ for remoteDirectory in remoteDirectories:
     os.makedirs("derived/{}".format(now.strftime("%Y-%m-%d")+"-"+remoteDirectory.partition("@")[0], exist_ok=True))
     #declares batch_metadata
     batch_metadata = {
-    "uuid": remoteDirectory,
-  #  "issue": "https://github.com/braingeneers/internal/issues/{}".format(args.issue),
-    "notes": "\n".join([open(f).read() for f in glob.glob("{}/*.txt".format(remoteDirectory))])}
+    "uuid": now.strftime("%Y-%m-%d")+"-"+ remoteDirectory.partition("@")[0],
+    "issue": "https://github.com/braingeneers/internal/issues/{}".format(args.issue),
+    "notes": open("original/{}/batch.txt".format(now.strftime("%Y-%m-%d")+"-"+remoteDirectory.partition("@")[0])).read() if os.path.exists(
+        "original/{}/batch.txt".format(now.strftime("%Y-%m-%d")+"-"+remoteDirectory.partition("@")[0])) else ""}
 
-    experiments = []
+experiments = []
 
 
-# Add any metadata from json files to the batch metadata
-    #finds all of the json files in the remoteDirecroty and adds it to the the batch metadata
-    for f in glob.glob("{}/*.json".format(remoteDirectory)):
-        batch_metadata.update(json.load(open(f)))
 
+# # Add any metadata from json files to the batch metadata
+# for f in glob.glob("original/{}/*.json".format(args.uuid)):
+#     batch_metadata.update(json.load(open(f)))
 
 # Ingest each experiment and exhaust in normalized form into derived/<args.uuid>/
 
-    #print(remoteDirectory)
-    #gets all the paths from the remoteDirectory
-    for original_path in sorted(glob.glob("{}".format(remoteDirectory))):
+# Get a list of all the experiments by extracting unique prefixes
 
+
+
+for remoteDirectory in remoteDirectories:
+
+    rhds = sorted(glob.glob("original/{}/*.rhd".format(now.strftime("%Y-%m-%d")+"-"+remoteDirectory.partition("@")[0], exist_ok=True)))
+    experiment_names = sorted(set(
+        [re.findall(r"(.*?)\/(.*?)\/(.*?)_(\d{6}_\d{6}).rhd", s)[0][2] for s in rhds]))
+    print("Experiment names:", experiment_names)
+
+
+
+    for experiment_name in experiment_names:
+        print("Ingesting experiment", experiment_name)
 
         experiment_metadata = {}
 
-        experiment_metadata["name"] = os.path.basename(
-            os.path.normpath(original_path)).replace(" ", "-")
-        print("Experiment {} from {}".format(experiment_metadata["name"], original_path))
+        experiment_metadata["name"] = experiment_name
 
-    # Concatenate all *.txt files into a "notes" field
-        experiment_metadata["notes"] = "\n".join(
-            [open(f).read() for f in glob.glob("{}/*.txt".format(remoteDirectory))])
+    # Add <experiment_name>.txt to the notes field
+        if os.path.exists("original/{}/{}.txt".format(now.strftime("%Y-%m-%d")+"-"+remoteDirectory.partition("@")[0], experiment_name)):
+            experiment_metadata["notes"] = open(
+                "original/{}/{}.txt".format(now.strftime("%Y-%m-%d")+"-"+remoteDirectory.partition("@")[0], experiment_name)).read()
+        else:
+            experiment_metadata["notes"] = ""
 
-    # Walk through each *.rhd in the experiment sorted alphabetically and therefore by time
+    # Find all the rhd's that match the experiment name and walk through in sorted/time order
         experiment_metadata["samples"] = []
-        for sample_path in sorted(glob.glob("{}/*.rhd".format(original_path))):
-            print("|||||||||||||||||||||||||||||||||||||Sample {}".format(sample_path))
+        rhds = [p for p in sorted(glob.glob("original/{}/*.rhd".format(now.strftime("%Y-%m-%d")+"-"+remoteDirectory.partition("@")[0], exist_ok=True)))
+                if re.findall(r"(.*?)\/(.*?)\/(.*?)_(\d{6}_\d{6}).rhd", p)[0][2] == experiment_name]
 
+        print("Original rhd files to ingest:", rhds)
+
+        for sample_path in rhds:
+            print("Reading sample {}".format(sample_path))
+            
         # Try reading its *.rhd file stopping if there is an error. Any earlier
         # samples in the run will be retained and stored but with an "error" field
         # in the experiments metadata
+            data = read_data.read_data(sample_path)
             try:
                 data = read_data.read_data(sample_path)
             except Exception as e:
                 experiment_metadata["error"] = "{}: {}".format(sample_path, str(e))
                 print("ERROR: {}".format(experiment_metadata["error"]))
                 break
-
+                
             sample_metadata = {}
 
             sample_metadata["num_samples"] = data["num_amplifier_samples"]
@@ -135,17 +155,19 @@ for remoteDirectory in remoteDirectories:
             sample_metadata["name"] = os.path.splitext(
                 os.path.basename(sample_path).replace(" ", "-"))[0]
 
-        #sample_metadata["timestamp"] = datetime.datetime.strptime(
-         #   re.findall("_(\d+_\d+)$", sample_metadata["name"])[0], "%y%m%d_%H%M%S").isoformat()
+            sample_metadata["timestamp"] = datetime.datetime.strptime(
+                re.findall(r"_(\d{6}_\d{6})", sample_metadata["name"])[0], "%y%m%d_%H%M%S").isoformat()
+            print(sample_metadata["timestamp"])
 
-    #        if "timestamp" not in batch_metadata:
-     #           batch_metadata["timestamp"] = sample_metadata["timestamp"]
-
+            if "timestamp" not in batch_metadata:
+                batch_metadata["timestamp"] = sample_metadata["timestamp"]
+                
         # Add all the Intan metadata - < is a hack to exclude all sample data
             sample_metadata.update({k: v for k, v in data.items() if sys.getsizeof(v) < 2048})
 
             sample_metadata["original"] = sample_path
-            sample_metadata["derived"] = "derived/{}/{}.npy".format(now.strftime("%Y-%m-%d")+"-"+ remoteDirectory.partition("@")[0], sample_metadata["name"])
+            sample_metadata["derived"] = "derived/{}/{}.npy".format(now.strftime("%Y-%m-%d")+"-"+remoteDirectory.partition("@")[0], sample_metadata["name"])
+
         # Save the original un-modified np.uint16 to save space and so there is no loss of accuracy
             sample_metadata["units"] = "µV"
             sample_metadata["offset"] = 32768
@@ -153,26 +175,28 @@ for remoteDirectory in remoteDirectories:
             np.save(sample_metadata["derived"], data["amplifier_data"])
 
             experiment_metadata["samples"].append(sample_metadata)
-
-    # Add any metadata from json files to the experiment metadata
-        for f in glob.glob("{}/*.json".format(remoteDirectory)):
-            experiment_metadata.update(json.load(open(f)))
+            
+    # # Add any metadata from json files to the experiment metadata
+    # for f in glob.glob("{}/*.json".format(original_path)):
+    #     experiment_metadata.update(json.load(open(f)))
 
     # Save the meta data for this experiment
-        with open("derived/{}/{}.json".format(now.strftime("%Y-%m-%d")+"-"+ remoteDirectory.partition("@")[0], experiment_metadata["name"]), "w") as f:
+        with open("derived/{}/{}.json".format(now.strftime("%Y-%m-%d")+"-"+remoteDirectory.partition("@")[0], experiment_metadata["name"]), "w") as f:
             json.dump(experiment_metadata, f, sort_keys=True)
 
         experiments.append({
-            "path": "derived/{}/{}.json".format(now.strftime("%Y-%m-%d")+"-"+ remoteDirectory.partition("@")[0], experiment_metadata["name"]),
-       # "timestamp": experiment_metadata["samples"][0]["timestamp"]
+            "path": "derived/{}/{}.json".format(now.strftime("%Y-%m-%d")+"-"+remoteDirectory.partition("@")[0], experiment_metadata["name"]),
+            "timestamp": experiment_metadata["samples"][0]["timestamp"]
     })
-
+    
 # Add paths in timestamp order
-#batch_metadata["experiments"] = [
-   # e["path"] for e in sorted(experiments, key=lambda k: k['timestamp'], reverse=False)]
+batch_metadata["experiments"] = [
+    e["path"] for e in sorted(experiments, key=lambda k: k['timestamp'], reverse=False)]
 
-with open("derived/{}/metadata.json".format(now.strftime("%Y-%m-%d")+"-"+ remoteDirectory.partition("@")[0]), "w") as f:
+with open("derived/{}/metadata.json".format(now.strftime("%Y-%m-%d")+"-"+remoteDirectory.partition("@")[0], exist_ok=True), "w") as f:
     json.dump(batch_metadata, f, sort_keys=True)
+
+
 
 for removeThis in remoteDirectories:
     try:
@@ -184,9 +208,6 @@ for removeThis in remoteDirectories:
 for remoteDirectory in remoteDirectories:
     if (len(os.listdir("derived/{}".format(now.strftime("%Y-%m-%d")+"-"+ remoteDirectory.partition("@")[0])))==1):
         shutil.rmtree("derived/{}".format(now.strftime("%Y-%m-%d")+"-"+ remoteDirectory.partition("@")[0]))
-          
-      
-            
-            
-            
+
 print("Finished ingesting batch.")
+                                             
