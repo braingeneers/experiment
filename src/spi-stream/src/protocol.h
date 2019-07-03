ffbea0d3fc44600181a7e977430370e7bf727d8c
Kate Voitiuk <kvoitiuk@ucsc.edu>
2019-07-03 10:37:15 -0700

    Added JSON metadata example as comment into protocol.h for later implementation

--- src/spi-stream/src/protocol.h
+++ src/spi-stream/src/protocol.h
@@ -21,4 +21,30 @@
 //don't expext speed faster than 31MHz to work reliably on RasPi
 
 
+/*
+Metadata Output:
+{
+  "name": "name from experiment, usually set in Intan, or can be GUID",
+  "notes": "Free form notes, any start of line times are relative to start",
+  "timestamp": "UTC Start of experiment - may be before first block",
+  "sample_rate": "",
+  "offset": 32768,
+  "scaler": 0.195,
+  "units": "\u00b5V",
+  "channels": [{
+    "electrode_impedance_magnitude": 0.0,
+    "electrode_impedance_phase": 0.0,
+    "other parameters that can be extracted...",
+  }],
+  "blocks": [{
+    "timestamp": "UTC timestamp for this block, allows for gaps in recording...",
+    "num_samples": "Count of samples, here so we don't need to load in order to calculate",
+    "path": "path to derived file ie npy or tfrecord under the braingeneers archive",
+    "source": "optional path to original file, missing for simulated...",
+  }],
+}
+*/
+
+
+
 #endif  /*_MASTER_H*/
