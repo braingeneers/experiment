4641a596fabc57665b8afe3f9bb9928b4e1e6626
Nick Rezaee <37967537+resace3@users.noreply.github.com>
2020-07-01 08:50:40 -0500

    Update run_ingest.sh

--- ingest/cron_ingest/run_ingest.sh
+++ ingest/cron_ingest/run_ingest.sh
@@ -1,6 +1,8 @@
 
 #!/bin/bash
 
+# pulls the jobs for analysis that will be run after ingest
+
 source pull_git_jobs.sh
 
 python3 create_current_files_in_dir.py
@@ -17,6 +19,9 @@ echo ${current_files_in_dir[*]}
 
 A=${current_files_in_dir[@]};
 
+
+# the for loop goes through every zip file in s3://braingeneers-inbox/
+#-------------------------------------------------------------
 for NEW_EXPERIMENT in $(cat current_files_in_dir.txt )
 do
   echo $NEW_EXPERIMENT
@@ -26,9 +31,14 @@ do
   python3 download_from_prp.py
 
   python3 prp_ingest.py
-
+  
+  # After the prp_ingest.py code runs there s3://braingeneers/ephys/0000-00-00-e-info/original/ and s3://braingeneers/ephys/0000-00-00-e-info/derived/ are created
+  #-----------------------------------------------------------
   python3 upload_to_prp.py
+  
 
+  # jobs will get kicked off with source run_jobs.sh 
+  #--------------------------------
   source run_jobs.sh
 
 done
