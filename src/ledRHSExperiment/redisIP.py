0a75df97aef26d65e398c24157786d8b639ca596
Kate Voitiuk <kvoitiuk@ucsc.edu>
2021-01-06 23:33:27 -0800

    redisIP submits correct IP to redis now

--- src/ledRHSExperiment/redisIP.py
+++ src/ledRHSExperiment/redisIP.py
@@ -1,4 +1,5 @@
 import redis
+import os
 import socket
 
 def get_pi_name(filename):
@@ -8,7 +9,14 @@ def get_pi_name(filename):
 with open(os.path.expanduser('~pi/.redis/credentials')) as f:
 	redis_password = f.read().strip()
 	redis_client = redis.Redis(host='67.58.49.54', port=6379, password=redis_password)
-	name  = os.path.expanduser("~") 
-	ip = socket.gethostbyname(socket.gethostname()) 
-	redis_client.mset({name: ip})
+	gw = os.popen("ip -4 route show default").read().split()
+	s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+	s.connect((gw[2], 0))
+	ip = s.getsockname()[0]
+	gateway = gw[2]
+	host = socket.gethostname()
+	#print ("IP:", ip, " GW:", gateway, " Host:", host)
+	#print(host) 
+	#print(ip)
+	redis_client.mset({host: ip})
 
--- src/ledRHSExperiment/streamtest.py
+++ src/ledRHSExperiment/streamtest.py
@@ -4,8 +4,8 @@ import picamera
 import time 
 YOUTUBE="rtmp://a.rtmp.youtube.com/live2/" 
 KEY= "85f6-v3xc-jtvs-stps-a2s6"
-stream_cmd = 'ffmpeg -f h264 -r 25 -i - -itsoffset 5.5 -fflags nobuffer -f alsa -ac 1 -i hw:1,0 -vcodec copy -acodec aac -ac 1 -ar 8000 -ab 32k -map 0:0 -map 1:0 -strict experimental -f flv ' + YOUTUBE + KEY 
-stream_pipe = subprocess.Popen(stream_cmd, shell=True, stdin=subprocess.PIPE) 
+stream_cmd = 'ffmpeg -f h264 -r 25 -i - -itsoffset 5.5 -fflags nobuffer  -f alsa -ac 1 -vcodec copy  -acodec aac -ac 1 -i anullsrc  -ar 8000 -ab 32k -map 0:0 -map 1:0 -strict experimental -f flv ' + YOUTUBE + KEY 
+stream = subprocess.Popen(stream_cmd, shell=True, stdin=subprocess.PIPE) 
 camera = picamera.PiCamera(resolution=(640, 480), framerate=25) 
 try: 
   now = time.strftime("%Y-%m-%d-%H:%M:%S") 
